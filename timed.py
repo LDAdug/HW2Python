203c81734a373cff82aea4b0b9c1bcd495ce92ba
LDAdug <117054524+LDAdug@users.noreply.github.com>
2022-11-12 23:56:45 -0800

    Add files via upload

--- timed.py
+++ timed.py
@@ -4,13 +4,13 @@ def timeme(func):
     def wrap_func(*args, **kwargs):
         t1 = time.time()
         float(int(t1))
-        t12 = "{:.4f}".format(t1)
+        t12 = "{:.3f}".format(t1)
         result = func(*args, **kwargs)
 
         t2 = time.time()
         float(int(t2))
-        t22 = "{:.4f}".format(t2)
-        print(f'Total time {(t2-t1)}s')
+        t22 = "{:.3f}".format(t2)
+        print(f'Total time {(t22-t12)}s')
         return result
     return wrap_func
 
