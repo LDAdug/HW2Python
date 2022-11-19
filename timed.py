2b7ad219f1106e89bb00f9181495fe7e5551267b
LDAdug <117054524+LDAdug@users.noreply.github.com>
2022-11-18 16:21:28 -0800

    Add files via upload

--- timed.py
+++ timed.py
@@ -3,12 +3,12 @@ import time
 def timeme(func):
     def wrap_func(*args, **kwargs):
         t1 = time.time()
-        float(int(t1))
+        float(t1)
 
         result = func(*args, **kwargs)
 
         t2 = time.time()
-        float(int(t2))
+        float(t2)
 
         print(f"Total time %.2fs" % (t2-t1))
     return wrap_func
